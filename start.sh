0094f0e22ead2b69fa61665491ed4a46d1c86a2a
SarathLUN <lun.sarath@gmail.com>
2021-10-10 17:29:27 +0700

    add npm install in the starter script

--- start.sh
+++ start.sh
@@ -4,8 +4,10 @@ echo $CUR_DIR
 
 # start the documentation:
 cd "$CUR_DIR/docs/"
+npm install
 nohup ng serve --port 4201 -o &
 
 # start development server
 cd "$CUR_DIR/demo1/"
+npm install
 ng serve -o
